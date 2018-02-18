bb2d657bab54377ea38d5b6ca43581cb240e4565
Daria Kuznetsova <dukuznetsova@gmail.com>
2018-02-18 20:26:35 +0300

    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,14 +4,27 @@ from model.group import Group
 
 def test_edit_group_name(app):
     app.group.ensure_group_created(Group(name="test"))
-    app.group.edit_first_group(Group(name="new_name"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="new_name")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_edit_group_header(app):
-    app.group.ensure_group_created(Group(name="test"))
-    app.group.edit_first_group(Group(header="new_header"))
-
-
-def test_edit_group_footer(app):
-    app.group.ensure_group_created(Group(name="test"))
-    app.group.edit_first_group(Group(footer="new_footer"))
+# def test_edit_group_header(app):
+#     app.group.ensure_group_created(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.edit_first_group(Group(header="new_header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+#
+# def test_edit_group_footer(app):
+#     app.group.ensure_group_created(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.edit_first_group(Group(footer="new_footer"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
