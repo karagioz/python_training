38e94891583f5230f8f7e430cafa3879ebd1ec58
Daria Kuznetsova <dukuznetsova@gmail.com>
2018-03-04 22:45:10 +0300

    Реализована загрузка тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,10 +2,10 @@
 from model.contact import Contact
 
 
-def test_add_contact(app, data_contacts):
+def test_add_contact(app, json_contacts):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(data_contacts)
+    app.contact.create(json_contacts)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts.append(data_contacts)
+    old_contacts.append(json_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,26 +7,10 @@ def test_edit_group_name(app):
     app.group.ensure_group_created(Group(name="test"))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name="new_name")
+    group = Group(name="new_name", header="new_header", footer="new_footer")
     group.id = old_groups[index].id
     app.group.edit_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_edit_group_header(app):
-#     app.group.ensure_group_created(Group(name="test"))
-#     old_groups = app.group.get_group_list()
-#     app.group.edit_first_group(Group(header="new_header"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
-#
-#
-# def test_edit_group_footer(app):
-#     app.group.ensure_group_created(Group(name="test"))
-#     old_groups = app.group.get_group_list()
-#     app.group.edit_first_group(Group(footer="new_footer"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
