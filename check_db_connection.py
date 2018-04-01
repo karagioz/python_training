07021b40c162eed482cfecbd1ed5acbe9dc8a0df
Daria Kuznetsova <karagioz@yandex.ru>
2018-04-01 20:54:21 +0300

    File with list of required packages created

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 from fixture.orm import ORMFixture
 from model.group import Group
+from model.contact import Contact
 
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contacts_not_in_group(Group(id=278))
+    l = db.get_groups_with_any_contacts()
     for item in l:
         print(item)
     print(len(l))
